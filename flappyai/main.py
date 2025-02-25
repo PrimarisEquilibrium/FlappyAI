8906e81d01e1038c27eb65380a9da1c51adecfd7
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2025-02-24 20:13:21 -0500

    refactor: add sprites, background, and update relative coordinate values

--- flappyai/main.py
+++ flappyai/main.py
@@ -1,15 +1,19 @@
 import pygame
 
 # Screen dimensions
-SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 720
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = SCREEN_WIDTH * 1.3
 
 # Represents the "flappy" bird
 class Bird:
-    def __init__(self, x, y, bird_size):
+    def __init__(self, x, y, bird_scale):
         self.x = x
         self.y = y
-        self.bird_size = bird_size
+
+        self.bird = pygame.image.load("./assets/sprites/bird.png")
+        self.bird = pygame.transform.scale_by(self.bird, bird_scale)
+        self.ctr_x = self.bird.get_width() / 2
+        self.ctr_y = self.bird.get_height() / 2
 
         self.y_velocity = 0
         self.y_acceleration = 0.5
@@ -29,30 +33,39 @@ class Bird:
         self.y += self.y_velocity
 
         # !! Death triggers (boundaries for now)
-        if self.y + self.bird_size >= SCREEN_HEIGHT:
-            self.y = SCREEN_HEIGHT - self.bird_size
-        if self.y - self.bird_size < 0:
-            self.y = self.bird_size
+        if self.y + self.ctr_y >= SCREEN_HEIGHT:
+            self.y = SCREEN_HEIGHT - self.ctr_y
+        if self.y - self.ctr_y < 0:
+            self.y = self.ctr_y
             self.y_velocity = 0
     
     def draw(self, surface):
-        pygame.draw.circle(surface, "yellow", (self.x, self.y), 25)
+        surface.blit(self.bird, (self.x - self.ctr_x, self.y - self.ctr_y))
 
 def run():
     pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     running = True
     dt = 0
 
-    bird = Bird(50, 50, 25)
+    background = pygame.image.load("./assets/sprites/background.png")
+    background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    bird = Bird(SCREEN_WIDTH / 3, SCREEN_HEIGHT / 2, 1.25)
+    pygame.mouse.set_visible(0)
     while running:
         event_list = pygame.event.get()
         for event in event_list:
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    return
 
-        screen.fill("purple")
+        screen.fill((0, 0, 0))
+        screen.blit(background, (0, 0))
 
         bird.draw(screen)
         bird.update(event_list, dt)
