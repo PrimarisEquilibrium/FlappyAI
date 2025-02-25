b754bcac4e68d7e57e59eb70186ce175b6cb4cec
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2025-02-25 15:41:47 -0500

    feat: rename app caption and add icon

--- flappyai/main.py
+++ flappyai/main.py
@@ -6,6 +6,9 @@ from config import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def run():
     pygame.init()
+    pygame.display.set_caption("FlappyAI")
+    pygame.display.set_icon(pygame.image.load("./assets/sprites/bird.png"))
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     running = True
