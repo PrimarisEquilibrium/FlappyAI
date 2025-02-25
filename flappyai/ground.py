4a0757e7fa3767966f07a8b0f00b7fd6e2bf6ad2
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2025-02-25 15:32:46 -0500

    comment ground.py

--- flappyai/ground.py
+++ flappyai/ground.py
@@ -3,6 +3,8 @@ from config import SCREEN_WIDTH, SCREEN_HEIGHT
 
 ground_img = pygame.image.load("./assets/sprites/ground.png")
 ground_img = pygame.transform.scale_by(ground_img, 1.25)
+ground_width = ground_img.get_width()
+
 class Ground:
     def __init__(self, x, y):
         self.x = x
@@ -20,14 +22,17 @@ class GroundManager:
     
     def create_ground(self):
         furthest_x = 0
-        while furthest_x < SCREEN_WIDTH + ground_img.get_width():
+        # Keep adding the ground image until it fills the screen
+        while furthest_x < SCREEN_WIDTH + ground_width:
             self.ground_array.append(Ground(furthest_x, SCREEN_HEIGHT - 100))
-            furthest_x += ground_img.get_width()
+            furthest_x += ground_width
     
     def update(self, dt):
         for i, ground in enumerate(self.ground_array):
-            if ground.x + ground_img.get_width() < 0:
-                self.ground_array.append(Ground(self.ground_array[-1].x + ground_img.get_width(), SCREEN_HEIGHT - 100))
+            # Delete any ground images that cannot be seen on the screen anymore
+            # Then add a new one behind the screen
+            if ground.x + ground_width < 0:
+                self.ground_array.append(Ground(self.ground_array[-1].x + ground_width, SCREEN_HEIGHT - 100))
                 del self.ground_array[i]
             ground.update(dt)
     
